6cb7ead77244e824231efcc3a468461f710ba469
gsgallant <greg.gallant@yahoo.com>
2016-06-12 14:52:49 -0400

    bugs bugging me

---
+++ app/utils/helpers.js
@@ -0,0 +1,43 @@
+/*Include the Axios library for HTTP requests*/
+var axios = require('axios');
+
+/* NYT API Key*/
+var APIKey = "e05abfb61c5346fea5bf75beb92d714e";
+
+// Helper Functions (in this case the only one is runQuery)
+var helpers = {
+
+	// This will run our query.
+	runQuery: function(term, start, end){
+
+		// Adjust to get search terms in proper format
+		var term = term.trim();
+		var start = start.trim() + "0101";
+		var end = end.trim() + "1231";
+
+
+		// Run a query using Axios. Then return the results as an object with an array.
+		// See the Axios documentation for details on how we structured this with the params.
+		return axios.get('https://api.nytimes.com/svc/search/v2/articlesearch.json', {
+			params: {
+			    'api-key': APIKey,
+			    'q': term,
+			    'begin_date': start,
+			    'end_date': end			
+			}
+		})
+		.then(function(results){
+			console.log(results.data.response);
+
+			return results.data.response;
+
+		});
+
+
+
+	}
+}
+
+
+// We export the helpers function (which contains getGithubInfo)
+module.exports = helpers;
